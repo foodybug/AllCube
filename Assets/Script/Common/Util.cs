b601006a5ac7378300b87c34f8ca8d0f66e64f96
foodybug <foodybug@daum.net>
2026-07-14 07:57:27 +0900

    Isolate CubeLaser into dedicated C# file and reorganize entire Script directory layout into role-specific subdirectories (Manager, Cube, UI, Common, Player)

---
+++ Assets/Script/Common/Util.cs
@@ -0,0 +1,111 @@
+using UnityEngine;
+using System.Collections;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class Util : MonoBehaviour
+{
+	static public void MyDestroy(Object o)
+	{
+#if UNITY_EDITOR
+		//DestroyImmediate( o);
+		Destroy( o);
+#else
+		Destroy( o);
+#endif
+	}
+
+	static public void Quit()
+	{
+#if UNITY_EDITOR
+		EditorApplication.isPlaying = false;
+		EditorApplication.isPaused = false;
+#else
+	#if UNITY_ANDROID
+		/*
+		// dispose
+		System.Diagnostics.ProcessThreadCollection ptc = System.Diagnostics.Process.GetCurrentProcess().Threads;
+		foreach( System.Diagnostics.ProcessThread pt in ptc)
+		{
+			pt.Dispose();
+		}
+		System.Diagnostics.Process.GetCurrentProcess().Kill();
+		*/
+	#endif
+
+		Application.Quit();
+#endif
+	}
+}
+
+public class Coin : MonoBehaviour
+{
+    private Renderer m_renderer;
+    private Transform m_playerTransform;
+
+    // 보석 등급 변수 추가 (1~5)
+    public int grade = 1;
+
+    void Awake()
+    {
+        m_renderer = GetComponentInChildren<Renderer>();
+    }
+
+    void Start()
+    {
+        ApplyRandomTexture();
+        ApplyGradeScale();
+    }
+
+    void Update()
+    {
+        if (m_playerTransform == null)
+        {
+            Player player = FindAnyObjectByType<Player>();
+            if (player != null)
+            {
+                m_playerTransform = player.transform;
+            }
+        }
+
+        if (m_playerTransform != null)
+        {
+            transform.rotation = m_playerTransform.rotation;
+        }
+    }
+
+    public void ApplyGradeScale()
+    {
+        // grade 1 일 때 기본 크기 0.5f 에서 시작하여 등급 당 0.2f 씩 증가 (최대 1.3f)
+        float targetScale = 0.5f + (grade - 1) * 0.2f;
+        transform.localScale = new Vector3(targetScale, targetScale, targetScale);
+    }
+
+    public void ApplyRandomTexture()
+    {
+        if (m_renderer == null)
+        {
+            m_renderer = GetComponentInChildren<Renderer>();
+        }
+
+        if (m_renderer != null && MapManager.Instance != null)
+        {
+            Texture[] texCube = MapManager.Instance.texCube;
+            if (texCube != null && texCube.Length > 0)
+            {
+                int maxIdx = Mathf.Min(3, texCube.Length - 1);
+                if (maxIdx >= 0)
+                {
+                    int randIdx = Random.Range(0, maxIdx + 1);
+                    Texture targetTex = texCube[randIdx];
+
+                    if (targetTex != null)
+                    {
+                        m_renderer.material.mainTexture = targetTex;
+                    }
+                }
+            }
+        }
+    }
+}
