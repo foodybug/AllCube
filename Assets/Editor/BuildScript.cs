0ccb1a4f6af564a571d3b37ebdc1026e9a781c9a
foodybug <foodybug@daum.net>
2026-07-31 10:44:00 +0900

    Update Android Target SDK to API Level 35 (Android 15) to satisfy latest Google Play Console requirements

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -51,9 +51,9 @@ public class BuildScript
         PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.Android, "com.foodybug.allcube");
         PlayerSettings.bundleVersion = "1.0.0";
         PlayerSettings.defaultInterfaceOrientation = UIOrientation.Portrait;
-        // 안드로이드 호환성 API 레벨 설정 (최신 빌드 안정화: Min API 25 / Target API 34)
+        // 안드로이드 호환성 API 레벨 설정 (구글 최신 정책: Min API 25 / Target API 35)
         PlayerSettings.Android.minSdkVersion = AndroidSdkVersions.AndroidApiLevel25;
-        PlayerSettings.Android.targetSdkVersion = AndroidSdkVersions.AndroidApiLevel34;
+        PlayerSettings.Android.targetSdkVersion = (AndroidSdkVersions)35; // API Level 35 (Android 15) 최신 구글 플레이 정책 대응
         PlayerSettings.Android.bundleVersionCode = 1;
 
         // 스크립팅 백엔드 IL2CPP 전환 및 CPU 아키텍처 ARMv7 + ARM64 멀티 빌드 지원 (최신 64비트 단말 설치용)
